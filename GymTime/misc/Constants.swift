7e774a746c8302868781d153ed571a7840036d68
maf macbook <mafkhw@MacBook.local>
2018-05-21 20:16:22 +0100

    Started Tableview

---
+++ GymTime/misc/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GymTime
+//
+//  Created by maf macbook on 21/05/2018.
+//  Copyright © 2018 maf macbook. All rights reserved.
+//
+
+import Foundation
+struct TableViewIdentifiers {
+    static let timers = "TimersCell"
+}
